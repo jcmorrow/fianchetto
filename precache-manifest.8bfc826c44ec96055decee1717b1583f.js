12e5593ec4365b8cbdf80e8675b4175b8c8450ec
Josh Morrow <josh@jcmorrow.com>
2019-02-09 11:34:13 -0500

    Updates

---
+++ precache-manifest.8bfc826c44ec96055decee1717b1583f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24d7d89cc9488abbf0fa",
+    "url": "/fianchetto/static/css/main.e98bf5bb.chunk.css"
+  },
+  {
+    "revision": "24d7d89cc9488abbf0fa",
+    "url": "/fianchetto/static/js/main.24d7d89c.chunk.js"
+  },
+  {
+    "revision": "2b2f613da8905cbb3d15",
+    "url": "/fianchetto/static/js/1.2b2f613d.chunk.js"
+  },
+  {
+    "revision": "a974bc8eb6aaae5e9a4d",
+    "url": "/fianchetto/static/js/runtime~main.a974bc8e.js"
+  },
+  {
+    "revision": "40dfa1d046bfb216d034a88bb00e6e27",
+    "url": "/fianchetto/index.html"
+  }
+];
